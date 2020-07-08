bf0e77938a247ddca0fa4db636b5922329eaeade
harshitha-kumar99 <55646861+harshitha-kumar99@users.noreply.github.com>
2020-07-08 15:49:04 +0530

    Add files via upload
    Program to Print an Integer

---
+++ main (1).c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+int main() {   
+    int number;
+   
+    printf("Enter an integer: ");  
+    
+    // reads and stores input
+    scanf("%d", &number);
+
+    // displays output
+    printf("You entered: %d", number);
+    
+    return 0;
+}
